a72b3dbe97a9010eb39e20ffd9db998146f4b10e
asu1492 <77401045+asu1492@users.noreply.github.com>
2024-08-30 05:06:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Conditionally set the base path
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+};
 
 export default nextConfig;
