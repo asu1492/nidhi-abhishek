180a1e03b0ec7cb0566a7ce233ecb0df513790af
abhi1409 <asingh4@manh.com>
2024-09-12 18:36:47 +0530

    map styling

--- src/components/WeddingInvitation.tsx
+++ src/components/WeddingInvitation.tsx
@@ -53,9 +53,9 @@ const WeddingInvitation: FC<WeddingInvitationProps> = ({ setActiveComponent }) =
           <h3>Location Map</h3>
           <div className={styles.mapContainer}>
             <iframe
+              className={styles.mapIframe}
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5006.568489650371!2d74.6204422!3d25.9257995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39693f22885b187b%3A0x520014a2dcd514bd!2sHOTEL%20SAANJH%20PALACE!5e0!3m2!1sen!2sin!4v1635786584598!5m2!1sen!2sin"
               loading="lazy"
-              style={{ border: 0, width: "100%", height: "450px" }}
             ></iframe>
           </div>
         </div>
@@ -98,9 +98,9 @@ const WeddingInvitation: FC<WeddingInvitationProps> = ({ setActiveComponent }) =
           <h3>Location Map</h3>
           <div className={styles.mapContainer}>
             <iframe
+              className={styles.mapIframe}
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.2349993598773!2d77.62127107598891!3d12.956808987357167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae155082024bd3%3A0xbbd15fcab856b158!2sChopra%20Auditorium!5e0!3m2!1sen!2sin!4v1726143585524!5m2!1sen!2sin"
               loading="lazy"
-              style={{ border: 0, width: "100%", height: "450px" }}
             ></iframe>
           </div>
         </div>
